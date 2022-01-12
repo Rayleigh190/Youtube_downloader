82de8b0142a9ee68a21d7f899e0bd35a01cc9fa4
Rayleigh <rayleigh190@gmail.com>
2022-01-12 20:47:18 +0900

    Add download single vedio function

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+from pytube import YouTube
 from pytube import Playlist
 
-p = Playlist("url")
+print("1. 단일 영상 다운로드")
+print("2. 플레이 리스트 다운로드")
+sel_num = input("숫자를 입력하세요: ")
 
-print(f'Downloading: {p.title}')
+def download_video():
+    url = input("영상 주소를 입력하세요: ")
+    yt = YouTube(url)
+    print(f'Downloading: {yt.title}')
+    yt.streams.get_by_itag(22).download('./video')
+    print("sucess!")
 
-cnt = 0
-for video in p.videos:
-    if cnt > 28:
+
+def download_playlist():
+    url = input("플레이 리스트 주소를 입력하세요: ")
+    p = Playlist(url)
+    print(f'Downloading: {p.title}')
+    for video in p.videos:
         video.streams.get_by_itag(22).download('./video')
-    cnt = cnt + 1
+    print("sucess!")
+
+if sel_num == '1':
+    download_video()
+else:
+    download_playlist()
 
 # pl.download_all() #파이썬 파일과 같은 위치
 #pl.download_all('./video') #저장위치
